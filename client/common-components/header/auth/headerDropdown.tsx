a336fd9f4faba65c1fb3889430a2664ad21fdefb
Bernt Christian Egeland <cbegelan@gmail.com>
2021-08-17 13:04:51 +0200

    lint

--- client/common-components/header/auth/headerDropdown.tsx
+++ client/common-components/header/auth/headerDropdown.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { authActions } from '../../../common-actions/auth.actions';
