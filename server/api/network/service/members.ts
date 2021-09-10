87dabc684709df046c03f13da9d331d0471973f5
Bernt Christian Egeland <cbegelan@gmail.com>
2021-09-10 12:10:01 +0200

    improved lastseen

--- server/api/network/service/members.ts
+++ server/api/network/service/members.ts
@@ -7,8 +7,8 @@ export const psql_updateOrCreateMembers = (members: Array<any>) =>
     if (!members.length) resolve([]);
 
     map(members, async (member: any) => {
-      const fetchMember = await NetworkService.network_members.findFirst(member.id);
-
+      const fetchMember = await NetworkService.network_members.findFirst({ where: { id: member.id, nwid: member.nwid } });
+      console.log(fetchMember);
       const updateMember = await NetworkService.network_members.updateMany({
         where: {
           nwid: member.nwid,
