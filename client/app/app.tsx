4c45aedf272615fddcab9d10af83c86368a15c11
Bernt Christian Egeland <cbegelan@gmail.com>
2022-05-10 12:39:11 +0200

    analytics

--- client/app/app.tsx
+++ client/app/app.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
+import ReactGA from "react-ga4";
 import Routes from './routes';
 // AUTH
 import { setAccessToken } from 'client/utils/accessToken';
 
 // @ts-ignore
 import config from 'config';
+import { withRouter } from 'react-router';
 
-const App: React.FC = (): JSX.Element => {
+ReactGA.initialize("G-X16M3DW3G6");
+
+const App: React.FC = ({location}:any): JSX.Element => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
     // Open sidebar on pageload
     document.body.classList.remove('sidebar-minimized');
     document.body.classList.remove('brand-minimized');
@@ -31,4 +36,4 @@ const App: React.FC = (): JSX.Element => {
   return <Routes />;
 };
 
-export default App;
+export default withRouter(App);
