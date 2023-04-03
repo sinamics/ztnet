ba4cb8c9eb8cc5a041dab164ce11c19bdd77896d
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-03 21:59:45 +0200

    modal

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -9,3 +9,33 @@ export const useSidebarStore = create<StoreI>((set) => ({
   open: false,
   toggle: () => set((state) => ({ open: !state.open })),
 }));
+
+type IcallModal = {
+  title: string;
+  description: string;
+  yesAction: () => void;
+  disableClickOutside: boolean;
+};
+
+type ModalStore = {
+  toggleModal?: () => void;
+  isOpen?: boolean;
+  disableClickOutside: boolean;
+  callModal: (IcallModal) => void;
+  description?: string;
+  title?: string;
+  yesAction?: () => void;
+  closeModal?: () => void;
+};
+
+export const useModalStore = create<ModalStore>((set, get) => ({
+  isOpen: false,
+  disableClickOutside: false,
+  closeModal: () => set(() => ({ isOpen: false })),
+  toggleModal: () => set((state) => ({ isOpen: !state.isOpen })),
+  callModal: (data: IcallModal) => {
+    const { toggleModal } = get();
+    toggleModal();
+    set({ ...data });
+  },
+}));
