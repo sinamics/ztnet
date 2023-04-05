565792ea52b31f0cd950d230918b03bb5ebdf5b8
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-05 22:52:24 +0000

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,14 +22,14 @@ const config = {
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
-      "warn",
+      "error",
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "no-console": "warn", // Add this line to warn about console.log usage
+    "no-console": "error", // Add this line to warn about console.log usage
   },
 };
 
