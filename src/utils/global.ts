1b2977f86e1407b71bdd234206efc00e42612d53
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-06 22:14:06 +0000

    env variables

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,6 +1,8 @@
 // Get the title from the environment variable NEXT_PUBLIC_SITE_NAME.
-// If it is not set, use the default value "Next ZTnet".
-export const globalSiteTitle =
-  process.env.NEXT_PUBLIC_SITE_NAME || "Next ZTnet";
 
-export const globalSiteVersion = process.env.NEXT_PUBLIC_APP_VERSION || null;
+import { env } from "~/env.mjs";
+
+// If it is not set, use the default value "Next ZTnet".
+export const globalSiteTitle = env.NEXT_PUBLIC_SITE_NAME || "Next ZTnet";
+
+export const globalSiteVersion = env.NEXT_PUBLIC_APP_VERSION || null;
