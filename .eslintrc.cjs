ac1ad18814e34c79fdadf14f018d9d115db1be5e
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-06 10:56:32 +0000

    tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,11 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest-dom/recommended",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "error",
