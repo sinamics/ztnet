c3c9f7dfafbb13df9f2886ef93a186186cf8b9fc
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-07 07:53:35 +0000

    client env

--- init-db.sh
+++ init-db.sh
@@ -8,9 +8,9 @@ cmd="$@"
 cat << EOF > .env
 DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public
 ZT_ADDR=${ZT_ADDR}
-NEXT_PUBLIC_SITE_NAME=${NEXT_PUBLIC_SITE_NAME}
-NEXT_PUBLIC_APP_VERSION=${NEXT_PUBLIC_APP_VERSION}
 EOF
+
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_APP_VERSION#$NEXT_PUBLIC_APP_VERSION#g"
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
