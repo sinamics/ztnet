38c3814a5388196ccf778ee88016c2eb964cc5d9
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-07 09:01:31 +0000

    client env

--- init-db.sh
+++ init-db.sh
@@ -10,7 +10,33 @@ DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}
 ZT_ADDR=${ZT_ADDR}
 EOF
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_NAME#${NEXT_PUBLIC_SITE_NAME}#g"
+# config
+envFilename='.env.production'
+nextFolder='/app/.next/'
+function apply_path {
+  # read all config file  
+  while read line; do
+    # no comment or not empty
+    if [ "${line:0:1}" == "#" ] || [ "${line}" == "" ]; then
+      continue
+    fi
+    
+    # split
+    configName="$(cut -d'=' -f1 <<<"$line")"
+    configValue="$(cut -d'=' -f2 <<<"$line")"
+    # get system env
+    envValue=$(env | grep "^$configName=" | grep -oe '[^=]*$');
+    
+    # if config found
+    if [ -n "$configValue" ] && [ -n "$envValue" ]; then
+      # replace all
+      echo "Replace: ${configValue} with: ${envValue}"
+      find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
+    fi
+  done < $envFilename
+}
+
+apply_path
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
