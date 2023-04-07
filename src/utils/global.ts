1b362469fab4fc05eb12174beb7784fb06cb038a
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-04-07 13:09:26 +0200

    site name

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -3,6 +3,6 @@
 import { env } from "~/env.mjs";
 
 // If it is not set, use the default value "Next ZTnet".
-export const globalSiteTitle = "Next ZTnet";
+export const globalSiteTitle = "ztnet";
 
 export const globalSiteVersion = env.NEXT_PUBLIC_APP_VERSION || null;
