557eeef05f64bc78a8b239bd6040dc9897f11389
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-07-24 10:43:08 +0000

    network table pagination

---
+++ src/components/elements/debouncedInput.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState, type InputHTMLAttributes } from "react";
+
+// A debounced input react component
+export const DebouncedInput = ({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<InputHTMLAttributes<HTMLInputElement>, "onChange">) => {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
+  return (
+    <input
+      {...props}
+      className="input input-bordered input-sm w-full max-w-xs"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
