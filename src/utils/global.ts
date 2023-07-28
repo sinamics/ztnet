951ca54e4e6b7c465aa38988506746c61ece31cb
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-07-28 11:57:58 +0000

    changed repo name

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -2,7 +2,7 @@
 
 import { env } from "~/env.mjs";
 
-// If it is not set, use the default value "Next ZTnet".
-export const globalSiteTitle = "Next ZTnet";
+// If it is not set, use the default value "ZTnet".
+export const globalSiteTitle = "ZTNET";
 
 export const globalSiteVersion = env.NEXT_PUBLIC_APP_VERSION || null;
