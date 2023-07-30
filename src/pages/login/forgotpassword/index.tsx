449b23684956de5e20fef9bbd57615b379c08d81
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-07-30 18:26:23 +0000

    updated tests

--- src/pages/login/forgotpassword/index.tsx
+++ src/pages/login/forgotpassword/index.tsx
@@ -6,7 +6,7 @@ import {
   type ErrorData,
   type ZodErrorFieldErrors,
 } from "~/types/errorHandling";
-import { type GetStaticPropsContext } from "next";
+import { type GetServerSidePropsContext } from "next";
 
 const ForgotPassword = () => {
   const router = useRouter();
@@ -109,14 +109,14 @@ const ForgotPassword = () => {
   );
 };
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       // You can get the messages from anywhere you like. The recommended
       // pattern is to put them in JSON files separated by locale and read
       // the desired one based on the `locale` received from Next.js.
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      messages: (await import(`../../locales/${context.locale}/common.json`))
+      messages: (await import(`~/locales/${context.locale}/common.json`))
         .default,
     },
   };
