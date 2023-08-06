50476d7a4ebec1a55b5ed2893a83a553ed12e0bd
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-08-06 19:22:00 +0000

    config

--- src/pages/user-settings/index.tsx
+++ src/pages/user-settings/index.tsx
@@ -6,64 +6,64 @@ import { type GetServerSidePropsContext } from "next";
 import { useTranslations } from "next-intl";
 
 const UserSettings = () => {
-	const router = useRouter();
-	const t = useTranslations("userSettings");
-	const { tab = "members" } = router.query;
-	//   const { t } = useTranslation();
-	interface ITab {
-		name: string;
-		value: string;
-		component: ReactElement;
-	}
+  const router = useRouter();
+  const t = useTranslations("userSettings");
+  const { tab = "members" } = router.query;
+  //   const { t } = useTranslation();
+  interface ITab {
+    name: string;
+    value: string;
+    component: ReactElement;
+  }
 
-	const tabs: ITab[] = [
-		{
-			name: t("tabs.account"),
-			value: "account",
-			component: <Account />,
-		},
-	];
+  const tabs: ITab[] = [
+    {
+      name: t("tabs.account"),
+      value: "account",
+      component: <Account />,
+    },
+  ];
 
-	const changeTab = async (tab: ITab) => {
-		await router.push({
-			pathname: "/user-settings",
-			query: { tab: tab.value },
-		});
-	};
-	return (
-		<div className="py-5">
-			<div className="tabs mx-auto w-full p-3 pb-10 sm:w-6/12">
-				{tabs.map((t) => (
-					<a
-						key={t.value}
-						onClick={() => void changeTab(t)}
-						className={`text-md tab tab-bordered ${
-							t.value === tab ? "tab-active" : ""
-						}`}
-					>
-						{t.name}
-					</a>
-				))}
-			</div>
-			{tabs.find((t) => t.value === tab)?.component}
-		</div>
-	);
+  const changeTab = async (tab: ITab) => {
+    await router.push({
+      pathname: "/user-settings",
+      query: { tab: tab.value },
+    });
+  };
+  return (
+    <div className="py-5">
+      <div className="tabs mx-auto w-full p-3 pb-10 sm:w-6/12">
+        {tabs.map((t) => (
+          <a
+            key={t.value}
+            onClick={() => void changeTab(t)}
+            className={`text-md tab tab-bordered ${
+              t.value === tab ? "tab-active" : ""
+            }`}
+          >
+            {t.name}
+          </a>
+        ))}
+      </div>
+      {tabs.find((t) => t.value === tab)?.component}
+    </div>
+  );
 };
 
 UserSettings.getLayout = function getLayout(page: ReactElement) {
-	return <LayoutAuthenticated>{page}</LayoutAuthenticated>;
+  return <LayoutAuthenticated>{page}</LayoutAuthenticated>;
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-	return {
-		props: {
-			// You can get the messages from anywhere you like. The recommended
-			// pattern is to put them in JSON files separated by locale and read
-			// the desired one based on the `locale` received from Next.js.
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-			messages: (await import(`../../locales/${context.locale}/common.json`))
-				.default,
-		},
-	};
+  return {
+    props: {
+      // You can get the messages from anywhere you like. The recommended
+      // pattern is to put them in JSON files separated by locale and read
+      // the desired one based on the `locale` received from Next.js.
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      messages: (await import(`../../locales/${context.locale}/common.json`))
+        .default,
+    },
+  };
 }
 export default UserSettings;
