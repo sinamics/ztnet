d606db1e7468916c4d28fd23d7298fe25f68257f
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-08-13 19:59:41 +0000

    usergroup migration

--- prisma/migrations/20230813195808_user_group/migration.sql
+++ prisma/migrations/20230813195808_user_group/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "AccessLevel" AS ENUM ('READ_ONLY', 'WRITE', 'ADMINISTRATIVE');
+
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "userGroupId" INTEGER;
 
@@ -7,6 +10,7 @@ CREATE TABLE "UserGroup" (
     "name" TEXT NOT NULL,
     "description" TEXT,
     "maxNetworks" INTEGER NOT NULL DEFAULT 5,
+    "accessLevel" "AccessLevel" NOT NULL DEFAULT 'WRITE',
     "isDefault" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "UserGroup_pkey" PRIMARY KEY ("id")
@@ -16,4 +20,4 @@ CREATE TABLE "UserGroup" (
 CREATE UNIQUE INDEX "UserGroup_name_key" ON "UserGroup"("name");
 
 -- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_userGroupId_fkey" FOREIGN KEY ("userGroupId") REFERENCES "UserGroup"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "User" ADD CONSTRAINT "User_userGroupId_fkey" FOREIGN KEY ("userGroupId") REFERENCES "UserGroup"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
