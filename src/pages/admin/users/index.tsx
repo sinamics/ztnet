3848da86277035ac601d62417e84008917e34363
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-08-13 19:04:49 +0000

    translations

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,30 +1,29 @@
+import { useTranslations } from "next-intl";
 import { type ReactElement } from "react";
 import { LayoutAuthenticated } from "~/components/layouts/layout";
 import { Accounts } from "~/components/modules/accountTable";
 import UserGroups from "~/components/modules/userGroups";
 
 const Users = () => {
+	const t = useTranslations("admin");
 	return (
 		<main className="mx-auto flex-col w-full bg-base-100 p-3 sm:w-6/12">
 			<div>
-				<p className="text-sm text-gray-400">Groups</p>
-				<div className="divider mt-0 text-gray-500"></div>
-			</div>
-			<div className="pb-5">
-				<p className="text-sm text-gray-500">
-					Here you can create a new user group to categorize and manage your
-					platform users more effectively. Assigning users to specific groups
-					can help streamline access rights, set specific limits, and apply
-					unique settings tailored for different sets of users.
+				<p className="text-sm text-gray-400">
+					{t("users.groups.sectionTitle")}
 				</p>
+				<div className="divider mt-0 text-gray-500"></div>
 			</div>
 			<div className="space-y-5 ">
 				<div className="w-full">
 					<UserGroups />
 				</div>
 			</div>
-
-			<div className="py-10">
+			<div className="pt-10 ">
+				<p className="text-sm text-gray-400">{t("users.users.sectionTitle")}</p>
+				<div className="divider mt-0 text-gray-500"></div>
+			</div>
+			<div className="w-full">
 				<Accounts />
 			</div>
 		</main>
