894c62b98c600e99940c56b650013a38b36e4d88
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-08-29 17:49:07 +0200

    docs

--- docs/ztnet/docusaurus.config.js
+++ docs/ztnet/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://ztnet.network',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ztnet/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
