0cdc9cc9779daf08c196a757494880f0b2400193
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-10-06 09:20:23 +0000

    sort

--- src/components/networkByIdPage/table/memberHeaderColumns.tsx
+++ src/components/networkByIdPage/table/memberHeaderColumns.tsx
@@ -60,6 +60,29 @@ const sortIP = (ip: string) => {
 		);
 	}
 };
+const sortingPhysicalIpAddress = (
+	rowA: Row<MemberEntity>,
+	rowB: Row<MemberEntity>,
+): number => {
+	const stripPort = (ip: string) => ip.split("/")[0];
+	const a = rowA.original.peers?.physicalAddress;
+	const b = rowB.original?.peers?.physicalAddress;
+
+	const convertToBigInt = (value: string | string[] | undefined): BigInt => {
+		if (Array.isArray(value)) {
+			return value.length ? sortIP(stripPort(value[0])) : BigInt(0);
+		} else {
+			return value?.length ? sortIP(stripPort(value)) : BigInt(0);
+		}
+	};
+
+	const numA = convertToBigInt(a);
+	const numB = convertToBigInt(b);
+
+	if (numA > numB) return 1;
+	if (numA < numB) return -1;
+	return 0;
+};
 const sortingIpAddress = (
 	rowA: Row<MemberEntity>,
 	rowB: Row<MemberEntity>,
@@ -233,7 +256,7 @@ export const MemberHeaderColumns = ({ nwid, central = false }: IProp) => {
 					sortDescFirst: true,
 					id: "physicalAddress",
 					sortUndefined: -1,
-					sortingFn: sortingIpAddress,
+					sortingFn: sortingPhysicalIpAddress,
 					cell: ({ getValue, row: { original } }) => {
 						if (central) {
 							const centralPhysicalAddress: string = original?.physicalAddress;
