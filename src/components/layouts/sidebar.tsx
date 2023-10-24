bf6d3f0951de460a10a0cf9b33a82c1f0c8039bc
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-10-24 18:40:58 +0200

    stroke-width

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -178,7 +178,7 @@ const Sidebar = (): JSX.Element => {
 											xmlns="http://www.w3.org/2000/svg"
 											fill="none"
 											viewBox="0 0 24 24"
-											stroke-width="1.5"
+											strokeWidth="1.5"
 											stroke="currentColor"
 											className="w-6 h-6"
 										>
