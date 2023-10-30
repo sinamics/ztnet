a926f0598b4ad0eeeb98bf4bde4def2ea3039e28
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-10-30 22:08:59 +0100

    transfer networks

--- prisma/seeds/update-user-id.ts
+++ prisma/seeds/update-user-id.ts
@@ -4,27 +4,45 @@ import { createId } from "@paralleldrive/cuid2";
 const prisma = new PrismaClient();
 
 export async function updateUserId() {
-	const users = await prisma.user.findMany();
+	const users = await prisma.user.findMany({
+		include: {
+			network: true,
+			options: true,
+		},
+	});
+
 	for (const user of users) {
 		if (Number.isInteger(Number(user.id.trim()))) {
 			const newId = createId();
-			// Create temporary unique email
-			const newEmail = `${user.email}_temp`;
 
-			// Create new user with new id and temporary email
+			// Create a new user record with new ID
+			// Create new user
+			const { options, network, ...otherUserFields } = user;
 			await prisma.user.create({
-				data: { ...user, id: newId, email: newEmail },
+				data: {
+					...otherUserFields,
+					id: newId,
+				},
 			});
 
-			// Delete original user
+			// Transfer networks to the new user
+			for (const net of network) {
+				await prisma.network.update({
+					where: { nwid: net.nwid },
+					data: { authorId: newId },
+				});
+			}
+
+			// Transfer UserOptions to the new user
+			if (options) {
+				await prisma.userOptions.create({
+					data: { ...options, userId: newId },
+				});
+			}
+
+			// Delete the old user record
 			await prisma.user.delete({
 				where: { id: user.id },
-			});
-
-			// Update back to original email
-			await prisma.user.update({
-				where: { id: newId },
-				data: { email: user.email },
 			});
 		}
 	}
