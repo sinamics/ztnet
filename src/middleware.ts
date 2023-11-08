9f6405815e6dd523b8f693ea2fd4c58658fb9760
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-11-08 19:58:17 +0000

    autodetect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
-export { default } from "next-auth/middleware";
-export const config = { matcher: ["/dashboard", "/networks"] };
-
 import { NextRequest, NextResponse } from "next/server";
+import { supportedLocales } from "./locales/lang";
+export const config = { matcher: ["/dashboard", "/networks"] };
 
 const PUBLIC_FILE = /\.(.*)$/;
 
@@ -13,10 +12,40 @@ export async function middleware(req: NextRequest) {
 	) {
 		return;
 	}
+
+	// Handle automatic locale detection
 	if (req.nextUrl.locale === "default") {
-		const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
-		return NextResponse.redirect(
-			new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url),
-		);
+		const fallbackLocale = "en";
+		const acceptLanguageHeader = req.headers.get("accept-language");
+		let preferredLocale = fallbackLocale;
+
+		if (acceptLanguageHeader) {
+			// Parse the Accept-Language header and sort by quality score
+			const locales = acceptLanguageHeader
+				.split(",")
+				.map((lang) => {
+					const [locale, qValue] = lang.trim().split(";q=");
+					return {
+						locale: locale.split("-")[0],
+						quality: qValue ? parseFloat(qValue) : 1,
+					};
+				})
+				.sort((a, b) => b.quality - a.quality); // Sort based on quality values
+			// Select the first supported locale with the highest quality score
+			const matchedLocale = locales.find((l) => supportedLocales.includes(l.locale));
+			if (matchedLocale) {
+				preferredLocale = matchedLocale.locale;
+			}
+		}
+
+		// Redirect to the preferred locale if it's different from the current one
+		if (preferredLocale !== req.nextUrl.locale) {
+			return NextResponse.redirect(
+				new URL(
+					`/${preferredLocale}${req.nextUrl.pathname}${req.nextUrl.search}`,
+					req.url,
+				),
+			);
+		}
 	}
 }
