a05b104ff67fab5a8c0f1cdcf7ae42ac96b53c5f
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-11-25 20:25:42 +0000

    chat icons

--- src/components/layouts/chatAside.tsx
+++ src/components/layouts/chatAside.tsx
@@ -145,25 +145,47 @@ const ChatAside = () => {
 		<>
 			{/* Chat Toggle Button */}
 			<button
-				className="fixed right-0 top-20 mr-4 z-10"
+				className={`w-14 z-10 fixed right-2 top-20 transition-all duration-150 ${
+					asideOpen ? "mr-72" : "w-0"
+				}`}
 				aria-label="Toggle chat"
 				onClick={() => toggleAside()}
 			>
-				{/* Replace with an actual chat icon */}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth="1.5"
-					stroke="currentColor"
-					className="w-6 h-6"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-					/>
-				</svg>
+				<div className="flex items-center">
+					{/* Replace with an actual chat icon */}
+					{asideOpen ? (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M8.25 4.5l7.5 7.5-7.5 7.5"
+							/>
+						</svg>
+					) : (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M15.75 19.5L8.25 12l7.5-7.5"
+							/>
+						</svg>
+					)}
+					<span className="text-xs">MSG</span>
+				</div>
 			</button>
 			{/* Chat Aside Panel */}
 			<aside
