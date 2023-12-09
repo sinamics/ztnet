2293dc1ed0ee31ff809fb03ddbb999b05802d146
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-12-09 10:48:13 +0000

    GET network, GET network_members

--- src/pages/api/v1/network/[id]/member/index.ts
+++ src/pages/api/v1/network/[id]/member/index.ts
@@ -1,7 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { getHTTPStatusCodeFromError } from "@trpc/server/http";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createNetworkService } from "~/server/api/services/networkService";
 import { prisma } from "~/server/db";
 import { decryptAndVerifyToken } from "~/utils/encryption";
 import rateLimit from "~/utils/rateLimit";
@@ -28,10 +27,7 @@ export default async function createNetworkHandler(
 	// create a switch based on the HTTP method
 	switch (req.method) {
 		case "GET":
-			await GET_userNetworks(req, res);
-			break;
-		case "POST":
-			await POST_createNewNetwork(req, res);
+			await GET_networkMembers(req, res);
 			break;
 		default: // Method Not Allowed
 			res.status(405).end();
@@ -39,38 +35,14 @@ export default async function createNetworkHandler(
 	}
 }
 
-const POST_createNewNetwork = async (req: NextApiRequest, res: NextApiResponse) => {
+const GET_networkMembers = async (req: NextApiRequest, res: NextApiResponse) => {
 	const apiKey = req.headers["x-ztnet-auth"] as string;
+	const networkId = req.query?.id as string;
 
-	let decryptedData: { userId: string; name: string };
-
-	// If there are users, verify the API key
-	try {
-		decryptedData = await decryptAndVerifyToken({ apiKey });
-	} catch (error) {
-		return res.status(401).json({ error: error.message });
+	// Check if the networkId exists
+	if (!networkId) {
+		return res.status(400).json({ error: "Network ID is required" });
 	}
-	const { name } = req.body;
-
-	const ctx = {
-		session: {
-			user: {
-				id: decryptedData.userId as string,
-			},
-		},
-		prisma,
-	};
-
-	const newNetworkId = await createNetworkService({
-		ctx,
-		input: { central: false, name },
-	});
-
-	return res.status(200).json(newNetworkId);
-};
-
-const GET_userNetworks = async (req: NextApiRequest, res: NextApiResponse) => {
-	const apiKey = req.headers["x-ztnet-auth"] as string;
 
 	let decryptedData: { userId: string; name?: string };
 	try {
@@ -79,7 +51,7 @@ const GET_userNetworks = async (req: NextApiRequest, res: NextApiResponse) => {
 		return res.status(401).json({ error: error.message });
 	}
 
-	// If there are users, verify the API key
+	// assemble the context object
 	const ctx = {
 		session: {
 			user: {
@@ -89,24 +61,25 @@ const GET_userNetworks = async (req: NextApiRequest, res: NextApiResponse) => {
 		prisma,
 	};
 	try {
-		const networks = await prisma.user.findFirst({
+		const arr = [];
+		const networks = await prisma.network.findUnique({
 			where: {
-				id: decryptedData.userId,
+				nwid: networkId,
 			},
-			select: {
-				network: true,
+			include: {
+				networkMembers: true,
 			},
 		});
-		const arr = [];
-		// biome-ignore lint/correctness/noUnsafeOptionalChaining: <explanation>
-		for (const network of networks?.network) {
-			const ztControllerResponse = await ztController.local_network_detail(
+
+		for (const member of networks.networkMembers) {
+			const controllerMember = await ztController.member_details(
 				//@ts-expect-error
 				ctx,
-				network.nwid,
+				networkId,
+				member.id,
 				false,
 			);
-			arr.push(ztControllerResponse);
+			arr.push({ ...member, ...controllerMember });
 		}
 
 		return res.status(200).json(arr);
