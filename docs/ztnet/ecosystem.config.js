47527a8e49a552b6096a0e6c6dce3fb150d51d5b
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-12-12 17:31:35 +0100

    path

--- docs/ztnet/ecosystem.config.js
+++ docs/ztnet/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ztnet.network',
-      script: 'npm run serve -- --dir ~/ztnet_docs --port 3000 --host 0.0.0.0',
+      script: 'npm run serve -- --dir ~/ztnet_docs/build --port 3000 --host 0.0.0.0',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       env: {
         NODE_ENV: 'production',
