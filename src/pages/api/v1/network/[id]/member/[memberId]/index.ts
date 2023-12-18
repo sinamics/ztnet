8f8b678421b5c991f4f0322ca9f68b6c4cd45cb3
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-12-18 16:37:02 +0000

    added docs

--- src/pages/api/v1/network/[id]/member/[memberId]/index.ts
+++ src/pages/api/v1/network/[id]/member/[memberId]/index.ts
@@ -35,7 +35,7 @@ export default async function apiNetworkMembersHandler(
 	res: NextApiResponse,
 ) {
 	try {
-		await limiter.check(res, REQUEST_PR_MINUTE, "CREATE_USER_CACHE_TOKEN"); // 10 requests per minute
+		await limiter.check(res, REQUEST_PR_MINUTE, "UPDATE_USER_CACHE_TOKEN"); // 10 requests per minute
 	} catch {
 		return res.status(429).json({ error: "Rate limit exceeded" });
 	}
