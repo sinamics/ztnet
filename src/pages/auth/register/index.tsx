7867de878c37d4db6f031af1c5e582c340310df5
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2023-12-27 19:46:39 +0000

    public pages margin on small devices

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -20,7 +20,7 @@ const Register = () => {
 				<meta name="robots" content="noindex, nofollow" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className="flex min-h-screen flex-col">
+			<main className="flex min-h-screen flex-col m-5 sm:m-0">
 				{/* Main section */}
 				<div className="flex flex-grow items-center">
 					<div className="mx-auto flex">
