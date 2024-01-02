a6dfb1552cbda10bba03c1c1d9b37d9bec652ab7
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-02 09:40:45 +0100

    tty

--- install.ztnet/bash/ztnet.sh
+++ install.ztnet/bash/ztnet.sh
@@ -51,7 +51,7 @@ ask_question() {
 
     # Display the question with a check mark
     echo -ne "\033[32m✔\033[0m${YELLOW} $question (Yes/No) [Default: $default_answer]: ${NC}"
-    read -r reply -s < /dev/tty
+    read -r reply < /dev/tty
 
     # If no response is given, use the default answer
     if [[ -z "$reply" ]]; then
