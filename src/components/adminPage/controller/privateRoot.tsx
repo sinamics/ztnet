8779e40ed5501da2e40d37d060876af6b710f7b2
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-13 10:50:28 +0000

    added warning if custom port is used

--- src/components/adminPage/controller/privateRoot.tsx
+++ src/components/adminPage/controller/privateRoot.tsx
@@ -125,6 +125,38 @@ const PrivateRoot = () => {
 							<div className="space-y-5">
 								{getPlanet?.rootNodes?.map((node, i) => (
 									<div key={node.id} className="border border-primary rounded p-4 my-4">
+										{!node.endpoints.toString().includes("9993") ? (
+											<div role="alert" className="alert shadow-lg mb-5">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													className="stroke-info shrink-0 w-6 h-6"
+												>
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														strokeWidth="2"
+														d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+													></path>
+												</svg>
+												<div>
+													<h3 className="font-bold">
+														{t("controller.generatePlanet.customPortIsInUse")}
+													</h3>
+													<div className="text-xs">
+														{t.rich(
+															"controller.generatePlanet.customPortIsInUseDescription",
+															{
+																kbd: (content) => (
+																	<kbd className="kbd kbd-xs">{content}</kbd>
+																),
+															},
+														)}
+													</div>
+												</div>
+											</div>
+										) : null}
 										<p className="tracking-wide font-medium">Root #{i + 1}</p>
 										<p>
 											<strong>Comments:</strong> {node.comments}
