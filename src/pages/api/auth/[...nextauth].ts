20e8a71cf6de52f6d5e761bac7b2640655c25046
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-14 08:20:59 +0000

    set NEXTAUTH_URL dynamically

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions);
+export default function auth(req: NextApiRequest, res: NextApiResponse) {
+	const { host } = req.headers;
+	if (host && !!process.env.AUTH_TRUST_HOST) {
+		process.env.NEXTAUTH_URL = /localhost/.test(host || "") ? `http://${host}` : host;
+	}
+	return NextAuth(authOptions)(req, res);
+}
