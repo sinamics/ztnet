ca16cc5d42dd2b5ae47c6d8245ace2c0e1661c3b
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-14 09:27:33 +0000

    AUTH_TRUST_HOST

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-export default function auth(req: NextApiRequest, res: NextApiResponse) {
-	const { host } = req.headers;
-	if (host && !!process.env.ZTNET_TRUST_HOST) {
-		process.env.NEXTAUTH_URL = /localhost/.test(host || "") ? `http://${host}` : host;
-	}
-	return NextAuth(authOptions)(req, res);
-}
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+console.log("nextauth address: ", process.env.NEXTAUTH_URL);
+export default NextAuth(authOptions);
+
+// export default function auth(req: NextApiRequest, res: NextApiResponse) {
+// 	return NextAuth(authOptions)(req, res);
+// }
