3781f86f0ab74add0b231a8b2961a821ac1ddd3e
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-14 10:27:12 +0000

    auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-console.log("nextauth address: ", process.env.NEXTAUTH_URL);
 export default NextAuth(authOptions);
 
 // export default function auth(req: NextApiRequest, res: NextApiResponse) {
