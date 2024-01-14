3768808f5caf98aa0a907c387500716998de6c23
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-14 15:09:31 +0000

    config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,11 @@
-// import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
 
-// export default function auth(req: NextApiRequest, res: NextApiResponse) {
-// 	return NextAuth(authOptions)(req, res);
-// }
+export default function auth(req: NextApiRequest, res: NextApiResponse) {
+	const { host } = req.headers;
+	console.log(req.headers);
+	return NextAuth(authOptions)(req, res);
+}
