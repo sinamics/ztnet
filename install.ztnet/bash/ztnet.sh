b732a366c980b0392db15d4245fbdfb751494e00
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-01-15 19:54:40 +0100

    memory warning

--- install.ztnet/bash/ztnet.sh
+++ install.ztnet/bash/ztnet.sh
@@ -471,7 +471,7 @@ memory_warning() {
 
   # Check if total memory is less than or equal to 1024MB (1GB)
   if [ "$total_mem_mb" -le 1024 ]; then
-      echo "${YELLOW}Warning: Your system's total memory is only ${total_mem_mb}MB. If the installation process fails, it might be due to insufficient memory. Consider upgrading your memory if possible.${NC}"
+      printf "\n${YELLOW}Warning: Your system's total memory is only ${total_mem_mb}MB. If the installation process fails, it might be due to insufficient memory. Consider upgrading your memory if possible.${NC}"
       read -n 1 -s -r -p "Press space to continue" key
   fi
 }
