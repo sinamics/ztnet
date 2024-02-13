95dcb53958ca10b1a44d661199259eba4ded087a
Sam Li <samli@cloudeasy.net>
2024-02-13 14:59:44 +0800

    add Traditional Chinese translation

--- src/locales/lang.ts
+++ src/locales/lang.ts
@@ -1,9 +1,10 @@
-export const supportedLocales = ["en", "fr", "no", "zh", "es"];
+export const supportedLocales = ["en", "fr", "no", "zh", "zh-tw", "es"];
 export const languageNames = {
 	default: "System",
 	en: "English",
 	fr: "French",
 	no: "Norwegian",
 	zh: "Chinese",
+	"zh-tw": "Traditional Chinese",
 	es: "Spanish",
 };
