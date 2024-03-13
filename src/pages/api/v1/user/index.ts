cab7bd77429596306e1ef015d3450dc3226b10d7
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-03-13 17:08:58 +0100

    allow generation of apitoken

--- src/pages/api/v1/user/index.ts
+++ src/pages/api/v1/user/index.ts
@@ -1,11 +1,11 @@
-import { TRPCError } from "@trpc/server";
-import { getHTTPStatusCodeFromError } from "@trpc/server/http";
+import { $Enums } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 import { prisma } from "~/server/db";
 import { AuthorizationType } from "~/types/apiTypes";
 import { decryptAndVerifyToken } from "~/utils/encryption";
+import { handleApiErrors } from "~/utils/errors";
 import rateLimit from "~/utils/rateLimit";
 
 // Number of allowed requests per minute
@@ -37,6 +37,17 @@ export default async function createUserHandler(
 	}
 }
 
+interface ResponseType {
+	user: {
+		name: string;
+		email: string;
+		id: string;
+		expiresAt: Date;
+		role: $Enums.Role;
+	};
+	apiToken?: string; // Make apiToken an optional property
+}
+
 const POST_createUser = async (req: NextApiRequest, res: NextApiResponse) => {
 	const apiKey = req.headers["x-ztnet-auth"] as string;
 
@@ -44,63 +55,85 @@ const POST_createUser = async (req: NextApiRequest, res: NextApiResponse) => {
 
 	// Count the number of users in database
 	const userCount = await prisma.user.count();
-
-	if (userCount > 0) {
-		// If there are users, verify the API key
-		try {
+	try {
+		if (userCount > 0) {
+			// If there are users, verify the API key
 			await decryptAndVerifyToken({
 				apiKey,
 				requireAdmin: NEEDS_ADMIN,
 				apiAuthorizationType: AuthorizationType.PERSONAL,
 			});
-		} catch (error) {
-			return res.status(401).json({ error: error.message });
 		}
-	}
 
-	// Create context and caller
-	const ctx = await createTRPCContext({ req, res });
-	const caller = appRouter.createCaller(ctx);
+		// Create context and caller
+		const ctx = await createTRPCContext({ req, res });
+		const caller = appRouter.createCaller(ctx);
 
-	// get data from the post request
-	const { email, password, name, expiresAt } = req.body;
+		// get data from the post request
+		const { email, password, name, expiresAt, generateApiToken } = req.body;
 
-	if (userCount === 0 && expiresAt !== undefined) {
-		return res.status(400).json({ message: "Cannot add expiresAt for Admin user!" });
-	}
-	// Check if expiresAt is a valid date
-	if (expiresAt !== undefined) {
-		try {
+		if (userCount === 0 && expiresAt !== undefined) {
+			return res.status(400).json({ message: "Cannot add expiresAt for Admin user!" });
+		}
+
+		// Check if expiresAt is a valid date
+		if (expiresAt !== undefined) {
 			const date = new Date(expiresAt);
 			const isoString = date.toISOString();
 
 			if (expiresAt !== isoString) {
 				return res.status(400).json({ message: "Invalid expiresAt date" });
 			}
-		} catch (error) {
-			return res.status(400).json({ message: error.message });
 		}
-	}
 
-	try {
-		const user = await caller.auth.register({
-			email: email as string,
-			password: password as string,
-			name: name as string,
-			expiresAt: expiresAt as string,
-		});
+		const register = (await caller.auth.register({
+			email: email,
+			password: password,
+			name: name,
+			expiresAt: expiresAt,
+		})) as ResponseType;
 
-		return res.status(200).json(user);
-	} catch (cause) {
-		if (cause instanceof TRPCError) {
-			const httpCode = getHTTPStatusCodeFromError(cause);
-			try {
-				const parsedErrors = JSON.parse(cause.message);
-				return res.status(httpCode).json({ cause: parsedErrors });
-			} catch (_error) {
-				return res.status(httpCode).json({ error: cause.message.trim() });
+		if (register.user === undefined) {
+			return res.status(500).json({ message: "Internal server error" });
+		}
+		// creat fake context
+		const fakeCtx = {
+			session: {
+				user: {
+					id: register.user.id,
+				},
+			},
+			prisma,
+		};
+
+		// @ts-ignore fake context
+		const tokenCaller = appRouter.createCaller(fakeCtx);
+		let apiToken: string | undefined;
+		if (generateApiToken !== undefined) {
+			if (typeof generateApiToken !== "boolean") {
+				return res.status(400).json({ message: "generateApiToken must be a boolean" });
+			}
+			// generate a new API token
+			if (generateApiToken) {
+				const tokenResponse = await tokenCaller.auth.addApiToken({
+					name: "Generated Token via API",
+					apiAuthorizationType: ["PERSONAL", "ORGANIZATION"],
+					daysToExpire: "1",
+				});
+				if (tokenResponse.token !== undefined) {
+					apiToken = tokenResponse.token;
+				}
 			}
 		}
-		return res.status(500).json({ message: "Internal server error" });
+
+		const response: ResponseType = { user: register.user };
+
+		if (apiToken !== undefined) {
+			response.apiToken = apiToken;
+		}
+
+		return res.status(200).json(response);
+	} catch (cause) {
+		return handleApiErrors(cause, res);
 	}
 };
