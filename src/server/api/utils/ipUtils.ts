f2221214285052aa55cc47bd3bdd1b932f3cd21d
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-05-08 20:36:13 +0000

    allow dns subdomain

--- src/server/api/utils/ipUtils.ts
+++ src/server/api/utils/ipUtils.ts
@@ -124,6 +124,6 @@ export function isValidIP(ip: string): boolean {
  * @returns `true` if the domain is valid, `false` otherwise.
  */
 export function isValidDomain(domain: string): boolean {
-	const domainRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/;
+	const domainRegex = /^(?!-)[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
 	return domainRegex.test(domain);
 }
