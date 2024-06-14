8ed06fcc7c901564e4fe71d5c5c7759b74768cc8
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-06-14 17:29:17 +0000

    calculate the headerbar height dynamically

---
+++ src/hooks/useUpdateVhAndHeaderHeight.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useCallback, useRef } from "react";
+
+const useUpdateVhAndHeaderHeight = () => {
+	const headerRef = useRef<HTMLDivElement>(null);
+
+	const updateVhAndHeaderHeight = useCallback(() => {
+		const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 48;
+		const vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+		document.documentElement.style.setProperty("--header-height", `${headerHeight}px`);
+	}, []);
+
+	useEffect(() => {
+		updateVhAndHeaderHeight();
+		window.addEventListener("resize", updateVhAndHeaderHeight);
+		return () => window.removeEventListener("resize", updateVhAndHeaderHeight);
+	}, [updateVhAndHeaderHeight]);
+
+	return headerRef;
+};
+
+export default useUpdateVhAndHeaderHeight;
