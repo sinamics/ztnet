13f754bfd34ba1108d2cd7d69ebcd8f2b8618599
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-04 18:33:51 +0000

    new 2fa require field

--- prisma/migrations/20240804183027_totp/migration.sql
+++ prisma/migrations/20240804183027_totp/migration.sql
@@ -1,4 +1,10 @@
 -- AlterTable
+ALTER TABLE "Invitation" ADD COLUMN     "require2FA" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Organization" ADD COLUMN     "require2FA" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
 ALTER TABLE "User" ADD COLUMN     "requestChangePassword" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "twoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "twoFactorSecret" TEXT;
