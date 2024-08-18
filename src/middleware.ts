728b39cc5ba8782d4a854d893c5f59be24a36f14
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-18 17:16:56 +0200

    host

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,26 +17,6 @@ export const config = {
 const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(req: NextRequest) {
-	// Dynamically determine the base URL from the X-Forwarded-Host and X-Forwarded-Proto headers
-	const forwardedHost = req.headers.get("x-forwarded-host");
-	const forwardedProto = req.headers.get("x-forwarded-proto");
-
-	let protocol: string;
-	let host: string;
-
-	if (forwardedHost && forwardedProto) {
-		// Use the forwarded headers if they exist
-		host = forwardedHost;
-		protocol = forwardedProto;
-	} else {
-		// Fallback to parsing the req.url
-		const url = new URL(req.url);
-		host = url.host;
-		protocol = url.protocol.slice(0, -1); // Remove the trailing colon from protocol
-	}
-
-	const baseUrl = `${protocol}://${host}`;
-
 	if (
 		req.nextUrl.pathname.startsWith("/_next") ||
 		req.nextUrl.pathname.includes("/api/") ||
@@ -44,6 +24,7 @@ export async function middleware(req: NextRequest) {
 	) {
 		return;
 	}
+
 	// Handle automatic locale detection
 	if (req.nextUrl.locale === "default") {
 		const fallbackLocale = "en";
@@ -51,7 +32,6 @@ export async function middleware(req: NextRequest) {
 		let preferredLocale = fallbackLocale;
 
 		if (acceptLanguageHeader) {
-			// Parse the Accept-Language header and sort by quality score
 			const locales = acceptLanguageHeader
 				.split(",")
 				.map((lang) => {
@@ -61,21 +41,22 @@ export async function middleware(req: NextRequest) {
 						quality: qValue ? parseFloat(qValue) : 1,
 					};
 				})
-				.sort((a, b) => b.quality - a.quality); // Sort based on quality values
-			// Select the first supported locale with the highest quality score
+				.sort((a, b) => b.quality - a.quality);
 			const matchedLocale = locales.find((l) => supportedLocales.includes(l.locale));
 			if (matchedLocale) {
 				preferredLocale = matchedLocale.locale;
 			}
 		}
-		// Redirect to the preferred locale if it's different from the current one
+
+		// Use the host from the request headers
+		const hostname = req.headers.get("host") || "";
+
 		if (preferredLocale !== req.nextUrl.locale) {
-			return NextResponse.redirect(
-				new URL(
-					`/${preferredLocale}${req.nextUrl.pathname}${req.nextUrl.search}`,
-					baseUrl,
-				),
+			const newUrl = new URL(
+				`/${preferredLocale}${req.nextUrl.pathname}${req.nextUrl.search}`,
+				`${req.nextUrl.protocol}//${hostname}`,
 			);
+			return NextResponse.redirect(newUrl);
 		}
 	}
 }
