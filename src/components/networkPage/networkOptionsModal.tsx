3dd0d1e8db413ef57bd18d1fde029aaed430b7df
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-20 08:12:16 +0000

    refactor modal

--- src/components/networkPage/networkOptionsModal.tsx
+++ src/components/networkPage/networkOptionsModal.tsx
@@ -7,6 +7,7 @@ import {
 	useTrpcApiErrorHandler,
 	useTrpcApiSuccessHandler,
 } from "~/hooks/useTrpcApiHandler";
+import { TransferNetworkToOrganization } from "./transferNetworkToOrganization";
 
 interface Iprops {
 	networkId: string;
@@ -20,28 +21,17 @@ const NetworkOptionsModal = ({ networkId }: Iprops) => {
 	const handleApiSuccess = useTrpcApiSuccessHandler();
 
 	const [action, setAction] = useState({ deleteNetwork: false, moveNetwork: false });
-	const [input, setInput] = useState({ organizationId: null });
 	const closeModal = useModalStore((state) => state.closeModal);
 
 	const { refetch: refetchNetwork } = api.network.getUserNetworks.useQuery({
 		central: false,
 	});
-	const { mutate: transferNetwork } = api.org.transferNetworkOwnership.useMutation({
-		onError: handleApiError,
-		onSuccess: handleApiSuccess({ actions: [refetchNetwork, closeModal] }),
-	});
+
 	const { mutate: deleteNetwork, isLoading: loadingDeleteNetwork } =
 		api.network.deleteNetwork.useMutation({
 			onError: handleApiError,
 			onSuccess: handleApiSuccess({ actions: [refetchNetwork, closeModal] }),
 		});
-
-	const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInput({
-			...input,
-			[e.target.name]: e.target.value,
-		});
-	};
 
 	if (loadingDeleteNetwork) {
 		return (
@@ -51,135 +41,61 @@ const NetworkOptionsModal = ({ networkId }: Iprops) => {
 		);
 	}
 	return (
-		<div>
-			<div className={cn({ "opacity-30": false })}>
-				<div className="grid grid-cols-4 items-start gap-4 space-y-10">
-					<div className="col-span-4 ">
-						<header className="text-md">{b("moveNetworkToOrganization")}</header>
-						<p className="text-sm text-gray-500">
-							{t("networkActionModal.moveNetwork.description")}
-						</p>
-
-						{action.moveNetwork ? (
-							<form>
-								<p className="text-sm text-red-600">
-									{t.rich("networkActionModal.moveNetwork.warningText", {
-										strong: (children) => <strong>{children}</strong>,
-										br: () => <br />,
-									})}
-								</p>
-
-								<input
-									type="text"
-									name="organizationId"
-									value={input.organizationId || ""}
-									onChange={inputHandler}
-									placeholder="Type the organization ID"
-									className="input input-bordered border-warning input-sm w-full max-w-xs my-2"
-								/>
-
-								<div className="flex gap-5">
-									<button
-										onClick={(e) => {
-											e.preventDefault();
-											transferNetwork({
-												nwid: networkId,
-												organizationId: input.organizationId,
-											});
-										}}
-										type="submit"
-										className="btn-sm btn-error btn w-2/6"
-									>
-										{b("imSure")}
-									</button>
-									<button
-										onClick={() =>
-											setAction((prev) => ({
-												...prev,
-												moveNetwork: !prev.moveNetwork,
-											}))
-										}
-										type="submit"
-										className="btn-sm btn w-2/6"
-									>
-										{b("cancel")}
-									</button>
-								</div>
-							</form>
-						) : (
-							<button
-								onClick={() =>
-									setAction((prev) => ({ ...prev, moveNetwork: !prev.moveNetwork }))
-								}
-								type="submit"
-								className="btn-sm btn btn-error btn-outline"
-							>
-								{b("moveNetwork")}
-							</button>
-						)}
-					</div>
-
-					<div className="col-span-4 ">
-						<header className="text-md">
-							{t("networkActionModal.deleteNetwork.title")}
-						</header>
-						<p className="text-sm text-gray-500">
-							{t("networkActionModal.deleteNetwork.description")}
-						</p>
-						{action.deleteNetwork ? (
-							<form className="space-y-5">
-								<p className="text-sm text-red-600">
-									{t.rich("networkActionModal.deleteNetwork.warningText", {
-										strong: (children) => <strong>{children}</strong>,
-										br: () => <br />,
-									})}
-								</p>
-								{/* <input
-									type="text"
-									name="networkId"
-									onChange={inputHandler}
-									placeholder="Type the network ID"
-									className="input input-bordered border-warning input-sm w-full max-w-xs my-2"
-								/> */}
-
-								<div className="flex gap-5">
-									<button
-										onClick={(e) => {
-											e.preventDefault();
-											deleteNetwork({ nwid: networkId });
-										}}
-										type="submit"
-										className="btn-sm btn-error btn w-2/6"
-									>
-										{b("imSure")}
-									</button>
-									<button
-										onClick={() =>
-											setAction((prev) => ({
-												...prev,
-												deleteNetwork: !prev.deleteNetwork,
-											}))
-										}
-										type="submit"
-										className="btn-sm btn w-2/6"
-									>
-										{b("cancel")}
-									</button>
-								</div>
-							</form>
-						) : (
-							<button
-								onClick={() =>
-									setAction((prev) => ({ ...prev, deleteNetwork: !prev.deleteNetwork }))
-								}
-								type="submit"
-								className="btn-sm btn btn-error btn-outline"
-							>
-								{b("deleteNetwork")}
-							</button>
-						)}
-					</div>
+		<div className="space-y-10">
+			<TransferNetworkToOrganization networkId={networkId} />
+			<div className={cn("space-y-5", { "opacity-30": false })}>
+				<div>
+					<header className="text-md">
+						{t("networkActionModal.deleteNetwork.title")}
+					</header>
+					<p className="text-sm text-gray-500">
+						{t("networkActionModal.deleteNetwork.description")}
+					</p>
 				</div>
+				{action.deleteNetwork ? (
+					<form className="space-y-5">
+						<p className="text-sm text-red-600">
+							{t.rich("networkActionModal.deleteNetwork.warningText", {
+								strong: (children) => <strong>{children}</strong>,
+								br: () => <br />,
+							})}
+						</p>
+						<div className="flex gap-5">
+							<button
+								onClick={(e) => {
+									e.preventDefault();
+									deleteNetwork({ nwid: networkId });
+								}}
+								type="submit"
+								className="btn-sm btn-error btn w-2/6"
+							>
+								{b("imSure")}
+							</button>
+							<button
+								onClick={() =>
+									setAction((prev) => ({
+										...prev,
+										deleteNetwork: !prev.deleteNetwork,
+									}))
+								}
+								type="submit"
+								className="btn-sm btn w-2/6"
+							>
+								{b("cancel")}
+							</button>
+						</div>
+					</form>
+				) : (
+					<button
+						onClick={() =>
+							setAction((prev) => ({ ...prev, deleteNetwork: !prev.deleteNetwork }))
+						}
+						type="submit"
+						className="btn-sm btn btn-primary"
+					>
+						{b("deleteNetwork")}
+					</button>
+				)}
 			</div>
 		</div>
 	);
