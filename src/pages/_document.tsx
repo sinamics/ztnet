43c6350be489ad2a14ba425ef57d7b83f178d289
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-21 06:59:42 +0000

    head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html>
-			<Head />
+			<Head>
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
