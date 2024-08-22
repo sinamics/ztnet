bcc515f2186df29328edfbeaea16173a915e4590
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-22 20:57:20 +0000

    new UserDevice model

---
+++ prisma/migrations/20240822202613_user_device/migration.sql
@@ -0,0 +1,29 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "deviceIsValid" BOOLEAN NOT NULL DEFAULT true;
+
+-- CreateTable
+CREATE TABLE "UserDevice" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "deviceType" TEXT NOT NULL,
+    "deviceId" TEXT NOT NULL,
+    "browser" TEXT NOT NULL,
+    "os" TEXT NOT NULL,
+    "lastActive" TIMESTAMP(3) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserDevice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserDevice_deviceId_key" ON "UserDevice"("deviceId");
+
+-- CreateIndex
+CREATE INDEX "UserDevice_userId_idx" ON "UserDevice"("userId");
+
+-- CreateIndex
+CREATE INDEX "UserDevice_deviceId_idx" ON "UserDevice"("deviceId");
+
+-- AddForeignKey
+ALTER TABLE "UserDevice" ADD CONSTRAINT "UserDevice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
