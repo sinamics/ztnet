a2c900ca92edaffcf5b10811fe3facace2acc202
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-08-24 12:03:19 +0000

    translations

--- src/components/auth/userDevices.tsx
+++ src/components/auth/userDevices.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import UAParser from "ua-parser-js";
 import type { UserDevice } from "@prisma/client";
 import cn from "classnames";
@@ -7,29 +6,35 @@ import Smartphone from "~/icons/smartphone";
 import Monitor from "~/icons/monitor";
 import Tablet from "~/icons/tablet";
 import { api } from "~/utils/api";
+import { useTranslations } from "next-intl";
 
 const formatLastActive = (date) => {
 	return new Date(date).toLocaleString("no-NO");
 };
 
-const DeviceInfo = ({ device, isCurrentDevice }) => (
-	<div>
-		<p
-			className={cn("font-semibold", {
-				"text-green-700": isCurrentDevice,
-			})}
-		>
-			{`${device.os} ${device.browser}`}
-		</p>
-		<p className="text-sm text-gray-500">
-			{formatLastActive(device.lastActive)}
-			{device.ipAddress && ` - ${device.ipAddress}`}
-		</p>
-		{isCurrentDevice && (
-			<p className="text-xs text-green-600 font-semibold">Active Now</p>
-		)}
-	</div>
-);
+const DeviceInfo = ({ device, isCurrentDevice }) => {
+	const t = useTranslations();
+	return (
+		<div>
+			<p
+				className={cn("font-semibold", {
+					"text-green-700": isCurrentDevice,
+				})}
+			>
+				{`${device.os} ${device.browser}`}
+			</p>
+			<p className="text-sm text-gray-500">
+				{formatLastActive(device.lastActive)}
+				{device.ipAddress && ` - ${device.ipAddress}`}
+			</p>
+			{isCurrentDevice && (
+				<p className="text-xs text-green-600 font-semibold">
+					{t("userSettings.account.userDevices.activeNow")}
+				</p>
+			)}
+		</div>
+	);
+};
 
 const DeviceIcon = ({ deviceType }: { deviceType: string }) => {
 	switch (deviceType.toLowerCase()) {
@@ -43,7 +48,7 @@ const DeviceIcon = ({ deviceType }: { deviceType: string }) => {
 };
 
 const ListUserDevices: React.FC<{ devices: UserDevice[] }> = ({ devices }) => {
-	const { data: session, status } = useSession();
+	const t = useTranslations();
 	const { refetch } = api.auth.me.useQuery();
 
 	const { mutate: deleteUserDevice, isLoading: deleteLoading } =
@@ -67,14 +72,6 @@ const ListUserDevices: React.FC<{ devices: UserDevice[] }> = ({ devices }) => {
 		});
 	}, []);
 
-	if (status === "loading") {
-		return <div>Loading...</div>;
-	}
-
-	if (status === "unauthenticated" || !session) {
-		return <div>Access Denied</div>;
-	}
-
 	const isCurrentDevice = (device: UserDevice) => {
 		return (
 			device.browser === currentDeviceInfo?.browser && device.os === currentDeviceInfo?.os
@@ -90,7 +87,9 @@ const ListUserDevices: React.FC<{ devices: UserDevice[] }> = ({ devices }) => {
 	return (
 		<div className="mx-auto">
 			<div className="flex justify-between">
-				<h1 className="text-md font-semibold mb-4">Connected Devices</h1>
+				<h1 className="text-md font-semibold mb-4">
+					{t("userSettings.account.userDevices.connectedDevices")}
+				</h1>
 				{/* <button className="btn btn-sm btn-error btn-outline">Logout All</button> */}
 			</div>
 			<div className="space-y-2 max-h-[500px] overflow-auto custom-scrollbar">
@@ -112,12 +111,12 @@ const ListUserDevices: React.FC<{ devices: UserDevice[] }> = ({ devices }) => {
 								className="btn btn-sm btn-primary"
 								onClick={() => deleteUserDevice({ deviceId: device.deviceId })}
 							>
-								Logg ut
+								{t("userSettings.account.userDevices.logout")}
 							</button>
 						</div>
 					))
 				) : (
-					<p>Ingen enheter funnet.</p>
+					<p>{t("userSettings.account.userDevices.noDevicesFound")}</p>
 				)}
 			</div>
 		</div>
