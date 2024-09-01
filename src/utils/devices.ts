eedfa02aa8f8d289fd4902ec497fda093ba05503
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-09-01 08:03:19 +0000

    invalidate user device cookie

--- src/utils/devices.ts
+++ src/utils/devices.ts
@@ -1,5 +1,9 @@
+import { serialize } from "cookie";
+import { GetServerSidePropsContext } from "next";
 import UAParser from "ua-parser-js";
 import { prisma } from "~/server/db";
+
+export const DEVICE_SALT_COOKIE_NAME = "next-auth.did-token";
 export interface ParsedUA {
 	deviceType: string;
 	browser: string;
@@ -51,3 +55,52 @@ export async function validateDeviceId(
 
 	return true;
 }
+
+/**
+ * Creates a user device cookie.
+ *
+ * @param res - The server response object.
+ * @param deviceId - The device ID.
+ */
+export const createDeviceCookie = (
+	res: GetServerSidePropsContext["res"],
+	deviceId: string,
+) => {
+	try {
+		res.setHeader("Set-Cookie", [
+			serialize(DEVICE_SALT_COOKIE_NAME, deviceId, {
+				httpOnly: true,
+				secure: false,
+				sameSite: "lax",
+				maxAge: 1 * 365 * 24 * 60 * 60, // 1 year
+				expires: new Date(Date.now() + 1 * 365 * 24 * 60 * 60 * 1000), // 1 year
+				path: "/",
+			}),
+		]);
+	} catch (error) {
+		console.error("Error creating device cookie:", error);
+	}
+};
+
+/**
+ * Deletes or invalidates the user device cookie.
+ *
+ * @param res - The server response object.
+ * @param deviceId - The ID of the device to delete the cookie for.
+ */
+export const deleteDeviceCookie = (res: GetServerSidePropsContext["res"]) => {
+	try {
+		res.setHeader("Set-Cookie", [
+			serialize(DEVICE_SALT_COOKIE_NAME, "", {
+				httpOnly: true,
+				secure: false,
+				sameSite: "lax",
+				maxAge: -1,
+				expires: new Date(0),
+				path: "/",
+			}),
+		]);
+	} catch (error) {
+		console.error("Error deleting device cookie:", error);
+	}
+};
