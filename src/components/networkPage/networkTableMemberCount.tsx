2d14cfdefeb1f80fedbadf80517faa202e5a10d0
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2024-12-17 20:30:10 +0100

    updated table

---
+++ src/components/networkPage/networkTableMemberCount.tsx
@@ -0,0 +1,7 @@
+export const NetworkTableMemberCount = ({ count }) => {
+	return (
+		<div className="tabular-nums inline-flex justify-end min-w-[3.5rem]">
+			<span>{count}</span>
+		</div>
+	);
+};
