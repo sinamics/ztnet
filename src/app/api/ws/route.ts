cbf9d91531a5de23b07a3525e483cce98c91bb72
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2025-01-06 08:13:43 +0000

    ws

--- src/app/api/ws/route.ts
+++ src/app/api/ws/route.ts
@@ -1,3 +1,10 @@
+export function GET() {
+	const headers = new Headers();
+	headers.set("Connection", "Upgrade");
+	headers.set("Upgrade", "websocket");
+	return new Response("Upgrade Required", { status: 426, headers });
+}
+
 export function SOCKET(
 	client: import("ws").WebSocket,
 	request: import("http").IncomingMessage,
@@ -6,11 +13,24 @@ export function SOCKET(
 	console.log("A client connected");
 
 	client.on("message", (message) => {
-		console.log("Received message:", message);
+		console.log("Received messagesssssssss:", message.toString());
 		client.send(message);
 	});
 
 	client.on("close", () => {
 		console.log("A client disconnected");
 	});
+}
+
+function createHelpers(
+	client: import("ws").WebSocket,
+	server: import("ws").WebSocketServer,
+) {
+	const send = (payload: unknown) => client.send(JSON.stringify(payload));
+	const broadcast = (payload: unknown) => {
+		if (payload instanceof Buffer) payload = payload.toString();
+		if (typeof payload !== "string") payload = JSON.stringify(payload);
+		for (const other of server.clients) if (other !== client) other.send(String(payload));
+	};
+	return { send, broadcast };
 }
--- src/components/layouts/authenticatedLayout.tsx
+++ src/components/layouts/authenticatedLayout.tsx
@@ -5,23 +5,38 @@
 // 3) Ensure you’re not adding or removing DOM elements based on store state
 //    before zustand is hydrated.
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "./footer";
 import Header from "./header";
 import Modal from "../shared/modal";
 import { useSidebarStore } from "~/store/sidebarStore";
 import useStore from "~/store/useStore";
 import Sidebar from "./sidebar";
+import { useWebSocket } from "next-ws/client";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
 	const store = useStore(useSidebarStore, (state) => state);
+	const ws = useWebSocket();
+	useEffect(() => {
+		async function onMessage(event: MessageEvent) {
+			const payload =
+				typeof event.data === "string" ? event.data : await event.data.text();
+			// const message = JSON.parse(payload);
+			console.log(await event.data.text());
+		}
 
+		ws?.addEventListener("message", onMessage);
+		return () => ws?.removeEventListener("message", onMessage);
+	}, [ws]);
 	// While rehydrating from localStorage, render nothing
 	// so the server DOM matches the client DOM.
 	// if (!store?.hydrated) return null;
 
 	return (
 		<div className="outer-container">
+			{/* <button onClick={() => ws?.send("Hello from client")}>
+				Send message to server
+			</button> */}
 			<Modal />
 			<Header />
 			<div className="flex">
