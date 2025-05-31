e1fce6495e0059f4e736cdd24c165f5d377ca6e5
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2025-06-01 00:25:38 +0200

    sidebar

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -418,6 +418,36 @@ const Sidebar = (): JSX.Element => {
 									<span className="ml-3">{t("organization")}</span>
 								</Link>
 							</li>
+							<li className="my-px">
+								<Link
+									href="/admin?tab=backup-restore"
+									className={`flex h-10 flex-row items-center rounded-lg px-3 
+              					${
+													router.pathname === "/admin" &&
+													router.query.tab === "backup-restore"
+														? "bg-gray-100 text-gray-700"
+														: "hover:bg-slate-700"
+												}`}
+								>
+									<span className="flex items-center justify-center text-lg text-gray-400">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="w-6 h-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+											/>
+										</svg>
+									</span>
+									<span className="ml-3">Backup-Restore</span>
+								</Link>
+							</li>
 						</>
 					) : null}
 					<li className="my-px">
