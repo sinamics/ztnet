65a264900a7452f206646ac55591d6c8e4892724
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2025-08-01 08:43:40 +0200

    migration

--- prisma/migrations/20250731132451_add_organization_notification_templates/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "OrganizationSettings" ADD COLUMN     "networkCreatedTemplate" JSONB,
-ADD COLUMN     "networkDeletedTemplate" JSONB,
-ADD COLUMN     "nodeAddedTemplate" JSONB,
-ADD COLUMN     "nodeDeletedTemplate" JSONB,
-ADD COLUMN     "permissionChangedTemplate" JSONB,
-ADD COLUMN     "userAddedTemplate" JSONB,
-ADD COLUMN     "userRemovedTemplate" JSONB;
--- prisma/migrations/20250801055644_node_deleted_notification/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OrganizationSettings" ADD COLUMN     "nodePermanentlyDeletedNotification" BOOLEAN DEFAULT false;
