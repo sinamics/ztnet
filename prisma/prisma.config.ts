fdfde82a7b2cb1b955b69f3690bd353ac034bb40
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2025-12-06 17:42:06 +0000

    upgraded to prisma version 7

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
 	schema: path.join(__dirname, "schema.prisma"),
+	migrations: {
+		path: path.join(__dirname, "migrations"),
+		seed: "tsx prisma/seed.ts",
+	},
 	datasource: {
 		url: process.env.DATABASE_URL ?? "",
 	},
