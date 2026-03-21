7d11964cb2ff9c97b6e8478adde963ef4929cfe1
Michael <41141752+schmidt-software@users.noreply.github.com>
2026-03-21 09:13:42 +0100

    Enhance network member schema and update POST method to include new fields (#852)
    * Enhance network member schema and update POST method to include new fields
    
    - Added optional fields to the PostBodySchema: activeBridge, capabilities, ipAssignments, noAutoAssignIps, ssoExempt, and tags.
    - Updated the POST_orgUpdateNetworkMember function to allow these new fields for network member updates.
    
    * Update PostBodySchema to change capabilities type to number and tags to a tuple of strings
    
    * Enhance network member schema by adding new fields
    
    - Introduced additional optional fields to the network member schema: activeBridge, capabilities, ipAssignments, noAutoAssignIps, ssoExempt, and tags.
    - Updated descriptions for clarity and consistency.
    
    * Refactor network member schema and update tags type
    
    - Adjusted the network member schema to ensure consistent formatting and improved readability.
    - Changed the type of tags in PostBodySchema from a tuple of strings to a tuple of numbers for better data representation.
    
    ---------
    
    Co-authored-by: Bernt Christian Egeland <bernt.christian.egeland@gmail.com>

--- src/pages/api/v1/org/[orgid]/network/[nwid]/member/[memberId]/_schema.ts
+++ src/pages/api/v1/org/[orgid]/network/[nwid]/member/[memberId]/_schema.ts
@@ -6,6 +6,12 @@ export const PostBodySchema = z
 		name: z.string().optional(),
 		description: z.string().optional(),
 		authorized: z.boolean().optional(),
+		activeBridge: z.boolean().optional(),
+		capabilities: z.array(z.number()).optional(),
+		ipAssignments: z.array(z.string()).optional(),
+		noAutoAssignIps: z.boolean().optional(),
+		ssoExempt: z.boolean().optional(),
+		tags: z.array(z.tuple([z.number(), z.number()])).optional(),
 	})
 	.strict();
 
