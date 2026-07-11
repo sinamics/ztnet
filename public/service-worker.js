8230dcb33004a14e3ce5fc76947f15cd4832efb5
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2026-07-11 10:18:38 +0200

    Node Name column blank on load + service worker precache error (#954)
    * resolve Node Name column staying blank until a route edit
    
    * correct precache asset path and make install resilient

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,22 @@ const CACHE_NAME = 'my-nextjs-pwa-cache';
 const urlsToCache = [
   '/',
   '/manifest.json',
-  '/icon.png'
+  '/ztnet_300x300.png'
   // Add other static assets to cache
 ];
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) =>
+      // Cache each asset individually so one missing/failed request doesn't
+      // reject the whole precache (which surfaces as an uncaught addAll error).
+      Promise.all(
+        urlsToCache.map((url) =>
+          cache.add(url).catch((err) => {
+            console.warn('SW precache skipped', url, err);
+          })
+        )
+      )
+    )
   );
 });
 self.addEventListener('fetch', (event) => {
